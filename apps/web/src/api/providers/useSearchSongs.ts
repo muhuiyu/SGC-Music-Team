c05df6b2cc3157a64706b720df1859fded5b04e5
muhuiyu <muhuiyu.grace@gmail.com>
2023-07-17 22:52:25 +0800

    Resolve errors

--- apps/web/src/api/providers/useSearchSongs.ts
+++ apps/web/src/api/providers/useSearchSongs.ts
@@ -1,48 +1,44 @@
-// export default function useSearchSongs() {
-//   const { data: songs, isFetching } = ({
-//     queryKey: [songsQueryKey],
-//     queryFn: async () => {
-//       const querySnapshot = await getDocs(collection(db, songsReference))
-//       return querySnapshot.docs.map(songFromSnapshot)
-//     },
-//   })
-
-//   const queryClient = useQueryClient()
-//   const mutation = useMutation({
-//     mutationFn: ({ songId, details }: { songId: Song['id']; details: Partial<Song> }) => {
-//       return updateDoc(doc(db, songsReference, songId), details)
-//     },
-//     onSuccess: () => {
-//       queryClient.invalidateQueries([songsQueryKey])
-//     },
-//   })
-//   const addMutation = useMutation({
-//     mutationFn: ({ details }: { details: Omit<Song, 'id'> }) => {
-//       return addDoc(collection(db, songsReference), details)
-//     },
-//     onSuccess: () => {
-//       queryClient.invalidateQueries([songsQueryKey])
-//     },
-//   })
-
-//   const updateSong = useCallback(
-//     (songId: Song['id'], details: Partial<Song>) => {
-//       mutation.mutate({ songId, details })
-//     },
-//     [mutation],
-//   )
-
-//   const addSong = useCallback(
-//     (details: Omit<Song, 'id'>) => {
-//       addMutation.mutate({ details })
-//     },
-//     [addMutation],
-//   )
-
-//   return {
-//     songs: songs ?? [],
-//     isLoading: isFetching,
-//     addSong,
-//     updateSong,
-//   }
-// }
+export default function useSearchSongs() {
+  //   const { data: songs, isFetching } = ({
+  //     queryKey: [songsQueryKey],
+  //     queryFn: async () => {
+  //       const querySnapshot = await getDocs(collection(db, songsReference))
+  //       return querySnapshot.docs.map(songFromSnapshot)
+  //     },
+  //   })
+  //   const queryClient = useQueryClient()
+  //   const mutation = useMutation({
+  //     mutationFn: ({ songId, details }: { songId: Song['id']; details: Partial<Song> }) => {
+  //       return updateDoc(doc(db, songsReference, songId), details)
+  //     },
+  //     onSuccess: () => {
+  //       queryClient.invalidateQueries([songsQueryKey])
+  //     },
+  //   })
+  //   const addMutation = useMutation({
+  //     mutationFn: ({ details }: { details: Omit<Song, 'id'> }) => {
+  //       return addDoc(collection(db, songsReference), details)
+  //     },
+  //     onSuccess: () => {
+  //       queryClient.invalidateQueries([songsQueryKey])
+  //     },
+  //   })
+  //   const updateSong = useCallback(
+  //     (songId: Song['id'], details: Partial<Song>) => {
+  //       mutation.mutate({ songId, details })
+  //     },
+  //     [mutation],
+  //   )
+  //   const addSong = useCallback(
+  //     (details: Omit<Song, 'id'>) => {
+  //       addMutation.mutate({ details })
+  //     },
+  //     [addMutation],
+  //   )
+  //   return {
+  //     songs: songs ?? [],
+  //     isLoading: isFetching,
+  //     addSong,
+  //     updateSong,
+  //   }
+}
